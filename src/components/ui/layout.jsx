162d029ae50bfd3684161e230ecd535d37385a45
jeel-patel-04 <itssjeel@gmail.com>
2025-11-14 19:36:02 +0530

    add logo

--- src/components/ui/layout.jsx
+++ src/components/ui/layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from './header'
 
 const Layout = ({children}) => {
   return (
     <div className='bg-linear-to-br from-background to-muted'>
-      header
+      <Header/>
       <main className='min-h-screen container mx-auto px-4 py-8'>
       {children}
       </main>
